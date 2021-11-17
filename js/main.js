840f43d0fab372d6703b142a0720cabc79bfebe4
wanbanka <hurt.stress@gmail.com>
2021-11-17 16:09:57 +0100

    UPDATE templating

--- js/main.js
+++ js/main.js
@@ -3,19 +3,14 @@ document.addEventListener("alpine:init", () => {
     Alpine.data("dropdown", () => ({
 
     open: false,
+    title: "Start here",
 
     toggle() {
         this.open = !this.open;
+        console.log(this.open);
     }
 
 }));
-
-Alpine.store('tabs', {
-
-    current: "first",
-    items: ["first", "second", "third"]
-
-});
 
 });
 
