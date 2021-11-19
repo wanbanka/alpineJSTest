a68988384ecf1725169b53e1f328fee8670c0f96
wanbanka <hurt.stress@gmail.com>
2021-11-19 15:04:47 +0100

    LEARN events

--- js/main.js
+++ js/main.js
@@ -6,6 +6,22 @@ document.addEventListener("alpine:init", () => {
 
     toggle() {
         this.open = !this.open;
+    },
+
+    pressEnter() {
+        console.log("Enter pressed");
+    },
+
+    pressShiftEnter() {
+        console.log("Shift + enter pressed");
+    },
+
+    validForm() {
+        console.log("Value of search: " + document.querySelector("input[name='search']").value);
+    },
+
+    displayFoo() {
+        console.log("Foo was dispatched");
     }
 
 }));
